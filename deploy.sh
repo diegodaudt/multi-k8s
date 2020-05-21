41baa699fe810727d4ded33babc56f11cd0248bc
Diego Daudt <diego.daudts@gmail.com>
2020-05-20 21:52:50 -0700

    Fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 docker build -t diegodaudt/multi-client:latest -t diegodaudt/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t diegodaudt/multi-server:latest -t diegodaudt/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t diegodaudt/multi-worker:latest -t diegodaudt/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push diegodaudt/multi-client:latest
 docker push diegodaudt/multi-server:latest
 docker push diegodaudt/multi-worker:latest
 
-diegodaudt/multi-client:$SHA
-diegodaudt/multi-server:$SHA
-diegodaudt/multi-worker:$SHA
+docker push diegodaudt/multi-client:$SHA
+docker push diegodaudt/multi-server:$SHA
+docker push diegodaudt/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=diegodaudt/multi-server:$SHA
